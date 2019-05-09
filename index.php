db5dcffd7a0e8c4e8e40d6c2f6e604cbea05f4be
VLubov <vlubov@team.amocrm.com>
2019-05-09 18:36:08 +0300

    I'm beginning

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+if (isset($_POST['enter_n'])) {
+	require_once($_SERVER['DOCUMENT_ROOT'].'/mygraid/functions.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].'/mygraid/my_logic.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Грейд. Задание 1</title>
+</head>
+<body>
+	<form method="POST" align="center">
+		<p>Введите n</p>
+		<input type="text" name="n">
+		<p><input type="submit" value="Сгенерировать" name="enter_n"></p>
+	</form>
+
+</body>
+</html>
