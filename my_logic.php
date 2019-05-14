a07c90dd9f45a92bae4d17dfab724210fc186263
VLubov <vlubov@team.amocrm.com>
2019-05-14 18:23:14 +0300

    90%

--- my_logic.php
+++ my_logic.php
@@ -1,31 +1,49 @@
 <?php 
 namespace FirstEx;
 
+function __autoload( $className ) {
+  $className = str_replace( "..", "", $className );
+  require_once( "/mygraid/classes/$className.php" );
+  echo "Loaded classes/$className.php<br>";
+}
+
+
 $hash = '5c88c3456481874aa6a2d5948f7b32e0dfdcb142';
 $mail = 'vlubov@team.amocrm.com';
 $sd = 'vlubov';
 auth_amo($mail, $hash, $sd);
 
-trait get_in_bd {
-	public function get_in_bd(){
-		$this->data;
-		$link = "https://vlubov.amocrm.ru/api/v2/{$this->type}";
-		$headers[] = "Accept: application/json";
+trait UseCurl {
+    public function use_curl($use_array_data){
+        $this->data;
+        $link = "https://vlubov.amocrm.ru/api/v2/{$this->type}";
+        $headers[] = "Accept: application/json";
 
-		 //Curl options
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-		curl_setopt($curl, CURLOPT_USERAGENT, "amoCRM-API-client-
+        //Curl options
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($curl, CURLOPT_USERAGENT, "amoCRM-API-client-
 		undefined/2.0");
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($this->data));
-		curl_setopt($curl, CURLOPT_URL, $link);
-		curl_setopt($curl, CURLOPT_HEADER,false);
-		curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__)."/cookie.txt");
-		curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__)."/cookie.txt");
-		$out = curl_exec($curl);
-		curl_close($curl);
-		$result = json_decode($out,TRUE);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        if ($use_array_data == true) {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($this->data));
+        }
+        curl_setopt($curl, CURLOPT_URL, $link);
+        curl_setopt($curl, CURLOPT_HEADER,false);
+        curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__)."/cookie.txt");
+        curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__)."/cookie.txt");
+        $out = curl_exec($curl);
+        curl_close($curl);
+        $result = json_decode($out,TRUE);
+        $this->result = $result;
+
+    }
+}
+
+Class GetInBD {
+    use UseCurl;
+	public function get_in_bd(){
+	    $this->use_curl(true);
 			switch ($this->type) {
 			case 'contacts':
 			$whatTheType = 'Контакт';
@@ -37,8 +55,8 @@ trait get_in_bd {
 			$whatTheType = 'Сделка';
 			break;
 		}
-		$result=$result['_embedded']['items'];
-		foreach($result as $v) {
+        $this->result=$this->result['_embedded']['items'];
+		foreach($this->result as $v) {
             if (is_array($v)) {
                     $s[] = $v;
                     $s = array_pop($s);
@@ -53,55 +71,12 @@ trait get_in_bd {
 
 
 
-
-class UpdateToAll {
-	public function get_up(){
-		$id_contact = '671949';
-		$data = array (
-		  'update' => 
-		  array (
-		    0 => 
-		    array (
-		      'id' => $id_contact,
-		      'updated_at' => '1557763680',
-		      'company_id' => '2078565',
-		      'leads_id' => 
-		      array (
-		        0 => '593201',
-		        1 => '594173',
-		        2 => '594709',
-		      ),
-		    ),
-		  ),
-		);
-
-		$link = "https://vlubov.amocrm.ru/api/v2/contacts";
-
-		$headers[] = "Accept: application/json";
-
-		 //Curl options
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-		curl_setopt($curl, CURLOPT_USERAGENT, "amoCRM-API-client-
-		undefined/2.0");
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-		curl_setopt($curl, CURLOPT_URL, $link);
-		curl_setopt($curl, CURLOPT_HEADER,false);
-		curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__)."/cookie.txt");
-		curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__)."/cookie.txt");
-		$out = curl_exec($curl);
-		curl_close($curl);
-		$result = json_decode($out,TRUE);
-	}
-}
-
 //$new_up = new UpdateToAll();
 //$new_up->get_up();
 
 
 class MultiSelect {
-	use get_in_bd;
+    use UseCurl;
 	public $data;
 	public function add_multi_select(){
 		
@@ -131,33 +106,20 @@ class MultiSelect {
 		    ),
 		  ),
 	);
-	$this->get_in_bd();	
+	$this->use_curl(true);
 	echo 'Мультисписок добвален' . '<br>'; 
 	}
 }
 
 class ContactUpdate {
+    use UseCurl;
 	public function get_info_user(){
-		$link = 'https://vlubov.amocrm.ru/api/v2/contacts/';
-
-		$headers[] = "Accept: application/json";
-
-		 //Curl options
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-		curl_setopt($curl, CURLOPT_USERAGENT, "amoCRM-API-client-
-		undefined/2.0");
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($curl, CURLOPT_URL, $link);
-		curl_setopt($curl, CURLOPT_HEADER,false);
-		curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__)."/cookie.txt");
-		curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__)."/cookie.txt");
-		$out = curl_exec($curl);
-		curl_close($curl);
-		$result = json_decode($out,TRUE);
-		$result = $result['_embedded']['items'];
+	    $this->type = 'contacts/';
+	    $this->date = '';
+        $this->use_curl(false);
+        $this->result = $this->result['_embedded']['items'];
 		$v = [];
-		foreach($result as $k) {
+		foreach($this->result as $k) {
 			  if(is_array($k))
 			  	$v[] = $k['id'];
 			  }
@@ -168,7 +130,6 @@ class ContactUpdate {
 		$this->get_update();
 		$this->get_info_user();
 		foreach ($this->ID as $key => $value) {
-		
 			$ar_keys = [];
 		foreach ($this->valuesid as $key => $v3) {
 			$ar_keys[] = $key; 
@@ -195,45 +156,15 @@ class ContactUpdate {
 		
 		}
 		$data = ['update' => $update];
-		$link = "https://vlubov.amocrm.ru/api/v2/contacts";
-
-		$headers[] = "Accept: application/json";
-
-		 //Curl options
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-		curl_setopt($curl, CURLOPT_USERAGENT, "amoCRM-API-client-
-		undefined/2.0");
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-		curl_setopt($curl, CURLOPT_URL, $link);
-		curl_setopt($curl, CURLOPT_HEADER,false);
-		curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__)."/cookie.txt");
-		curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__)."/cookie.txt");
-		$out = curl_exec($curl);
-		curl_close($curl);
-		$result = json_decode($out,TRUE);
-
+		$this->data = $data;
+	    $this->type = 'contacts';
+	    $this->use_curl(true)
 	}
 	public function get_update(){
-		$link = 'https://vlubov.amocrm.ru/api/v2/account?with=custom_fields';
-
-		$headers[] = "Accept: application/json";
-
-		 //Curl options
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-		curl_setopt($curl, CURLOPT_USERAGENT, "amoCRM-API-client-
-		undefined/2.0");
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($curl, CURLOPT_URL, $link);
-		curl_setopt($curl, CURLOPT_HEADER,false);
-		curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__)."/cookie.txt");
-		curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__)."/cookie.txt");
-		$out = curl_exec($curl);
-		curl_close($curl);
-		$result = json_decode($out,TRUE);
-		$valuesid = $result['_embedded']['custom_fields']['contacts']['83523']['enums'];
+	    $this->type = 'account?with=custom_fields';
+	    $this->data = '';
+        $this->use_curl(false);
+		$valuesid = $this->result['_embedded']['custom_fields']['contacts']['83523']['enums'];
 		$this->valuesid = $valuesid;
 		
 	}
@@ -247,8 +178,7 @@ class ContactUpdate {
 // $new_multiselect->add_multi_select();
 
 
-class Add {
-    use get_in_bd;
+class Add extends GetInBD {
         public function get_add($type, $data)
         {
             $this->data = $data;
@@ -265,21 +195,24 @@ class Add {
 }
 
 $n = $_POST['n'];
-$test_con = new Add();
-if ($n > 0 && $n < 10000) {
-	for ($i = 0; $i < $n; $i++){
-        $name = randString();
-	    $test_con->set_name($name);
-	    foreach ($test_con as $key => $value) {
-                $data['add'][] = $value;
-        }
-}
-} else {
-	echo 'Вы не можете добавить больше 10000 или меньше 1 записи одновременно';
-}
-$data = array_chunk($data['add'], 500, true);
-foreach ($data as $key => $value) {
-    $data = ['add' => $value];
 
-    $test_con->get_add('contacts',$data);
+
+    $new_es = new Add();
+{
+    if ($n > 0 && $n < 10000) {
+       for ($i = 0; $i < $n; $i++) {
+           $name = randString();
+           $new_es->set_name($name);
+           foreach ($new_es as $key => $value) {
+               $data['add'][] = $value;
+                }
+            }
+        } else {
+            echo 'Вы не можете добавить больше 10000 или меньше 1 записи одновременно';
+        }
+    $data = array_chunk($data['add'], 500, true);
+    foreach ($data as $key => $value) {
+        $data = ['add' => $value];
+        $new_es->get_add('contacts',$data);
+    }
 }
