7fcf0833670adc8df7e815a83f548c3810f2f74a
VLubov <vlubov@team.amocrm.com>
2019-05-17 19:27:16 +0300

    Final

--- Auth.php
+++ Auth.php
@@ -1,13 +1,14 @@
 <?php
 namespace AuthToAmo;
 require_once ($_SERVER['DOCUMENT_ROOT'].'/MyGraid/parametres.php');
-
+require_once ($_SERVER['DOCUMENT_ROOT'].'/MyGraid/my_logic.php');
+use SecondEx\UseCurl;
 /**
  * Class Auth
  * @package AuthToAmo
  */
 class Auth {
-
+use UseCurl;
 	public $hash;
 	public function __construct($hash, $mail, $sd) {
 		$this->hash = $hash;
@@ -19,29 +20,10 @@ class Auth {
             'USER_LOGIN'=>$this->mail, #Ваш логин (электронная почта)
             'USER_HASH'=>$this->hash #Хэш для доступа к API (смотрите в профиле пользователя)
     ];
-    $subdomain= $this->sd; #Наш аккаунт - поддомен
-    #Формируем ссылку для запроса
-    $link='https://'.$subdomain.'.amocrm.ru/private/api/auth.php?type=json';
-    /* Нам необходимо инициировать запрос к серверу. Воспользуемся библиотекой cURL (поставляется в составе PHP). Вы также
-    можете
-    использовать и кроссплатформенную программу cURL, если вы не программируете на PHP. */
-    $curl=curl_init(); #Сохраняем дескриптор сеанса cURL
-    #Устанавливаем необходимые опции для сеанса cURL
-    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
-    curl_setopt($curl,CURLOPT_URL,$link);
-    curl_setopt($curl,CURLOPT_CUSTOMREQUEST,'POST');
-    curl_setopt($curl,CURLOPT_POSTFIELDS,json_encode($user));
-    curl_setopt($curl,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
-    curl_setopt($curl,CURLOPT_HEADER,false);
-    curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
-    curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
-    curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
-    curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
-    $out=curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
-    $code=curl_getinfo($curl,CURLINFO_HTTP_CODE); #Получим HTTP-код ответа сервера
-    curl_close($curl); #Завершаем сеанс cURL
-    /* Теперь мы можем обработать ответ, полученный от сервера. Это пример. Вы можете обработать данные своим способом. */
+	$this->data = $user;
+    $this->use_curl(TRUE);
+    $code = $this->code;
+    $out = $this->out;
     $code=(int)$code;
     $errors=[
       301=>'Moved permanently',
--- my_logic.php
+++ my_logic.php
@@ -15,23 +15,32 @@ use MyAddField\AddField;
  */
 trait UseCurl {
     public function use_curl($use_array_data){
-        $link = "https://vlubov.amocrm.ru/api/v2/{$this->type}";
-        $headers[] = "Accept: application/json";
-
+        if ($use_array_data == TRUE) {
+            $link='https://'.$this->sd.'.amocrm.ru/private/api/auth.php?type=json';
+        } else {
+            $link = "https://vlubov.amocrm.ru/{$this->type}";
+        }
+        $headers = ['Content-Type: application/json'];
         //Curl options
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_RETURNTRANSFER,TRUE);
-        curl_setopt($curl, CURLOPT_USERAGENT, "amoCRM-API-client-
-		undefined/2.0");
+        curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client-
+		undefined/2.0');
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         if ($use_array_data == TRUE) {
-            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($this->data));
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($this->data));
+            curl_setopt($curl,CURLOPT_CUSTOMREQUEST,'POST');
+            curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
+            curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
         }
         curl_setopt($curl, CURLOPT_URL, $link);
         curl_setopt($curl, CURLOPT_HEADER,FALSE);
         curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__)."/cookie.txt");
         curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__)."/cookie.txt");
         $out = curl_exec($curl);
+        $this->out = $out;
+        $code=curl_getinfo($curl,CURLINFO_HTTP_CODE);
+        $this->code = $code;
         curl_close($curl);
         $result = json_decode($out,TRUE);
         $this->result = $result;
@@ -41,21 +50,22 @@ trait UseCurl {
 
 $auth = new Auth($hash, $mail, $sd);
 $auth->get_auth();
+pre($auth);
 
-switch ($_POST['type_es']){
-    case 'Контакт' :
-        $type_es = 'contacts';
-        break;
-    case 'Сделка' :
-        $type_es = 'leads';
-        break;
-    case 'Компания' :
-        $type_es = 'companies';
-        break;
-    case 'Покупатель' :
-        $type_es = 'customers';
-        break;
-}
-$update = new AddField;
-$update->get_date($type_es, $_POST['id'], $_POST['value_text']);
-echo "Данные добавленны";
+//switch ($_POST['type_es']){
+//    case 'Контакт' :
+//        $type_es = 'contacts';
+//        break;
+//    case 'Сделка' :
+//        $type_es = 'leads';
+//        break;
+//    case 'Компания' :
+//        $type_es = 'companies';
+//        break;
+//    case 'Покупатель' :
+//        $type_es = 'customers';
+//        break;
+//}
+//$update = new AddField;
+//$update->get_date($type_es, $_POST['id'], $_POST['value_text']);
+//echo "Данные добавленны";
