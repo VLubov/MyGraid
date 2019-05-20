2d08618e5b0422fcded2034df7a452dd94b5bec5
VLubov <vlubov@team.amocrm.com>
2019-05-20 18:05:05 +0300

    50%

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Тестируем виджет</title>
+</head>
+<body>
+<?php echo $_POST['id'] ?>
+</body>
+</html>
--- widget/script.js
+++ widget/script.js
@@ -1,49 +1,57 @@
 define(['jquery'], function($){
     var CustomWidget = function () {
-    	var self = this;
-		this.callbacks = {
-			render: function(){
-				console.log('render');
-				return true;
-			},
-			init: function(){
-				console.log('init');
-				return true;
-			},
-			bind_actions: function(){
-				console.log('bind_actions');
-				return true;
-			},
-			settings: function(){
-				return true;
-			},
-			onSave: function(){
-				alert('click');
-				return true;
-			},
-			destroy: function(){
-				
-			},
-			contacts: {
-					//select contacts in list and clicked on widget name
-					selected: function(){
-						console.log('contacts');
-					}
-				},
-			leads: {
-					//select leads in list and clicked on widget name
-					selected: function(){
-						console.log('leads');
-					}
-				},
-			tasks: {
-					//select taks in list and clicked on widget name
-					selected: function(){
-						console.log('tasks');
-					}
-				}
-		};
-		return this;
+        var self = this;
+        this.callbacks = {
+            render: function(){
+                console.log('render');
+                return true;
+            },
+            init: function(){
+                console.log('init');
+                return true;
+            },
+            bind_actions: function(){
+                console.log('bind_actions');
+                return true;
+            },
+            settings: function(){
+                return true;
+            },
+            onSave: function(){
+                alert('success');
+                return true;
+            },
+            destroy: function(){
+                
+            },
+            contacts: {
+                    //select contacts in list and clicked on widget name
+                    selected: function(){
+                        console.log('contacts');
+                    }
+                },
+            leads: {
+                    //select leads in list and clicked on widget name
+                    selected: function(){
+                        console.log('leads');
+                        let c_data = self.list_selected().selected;
+                        c_data = JSON.stringify(c_data);
+                        console.log(c_data);
+                    $.ajax({
+                      url: 'http://127.0.0.1/mygraid/index.php',
+                      type: "POST",
+                      data: {data: c_data},
+                        });
+                    }
+            },
+            tasks: {
+                    //select taks in list and clicked on widget name
+                    selected: function(){
+                        console.log('tasks');
+                    }
+                }
+        };
+        return this;
     };
 
 return CustomWidget;
